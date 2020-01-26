cb50adf0d9d96253db7f194963a3e1ea3025a469
xcgpseud <chris.evans92@hotmail.com>
2020-01-26 17:55:45 +0000

    admin stuff etc.

--- handlers/pester_handler.go
+++ handlers/pester_handler.go
@@ -8,24 +8,35 @@ import (
 )
 
 func PesterHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
+
+	if !helpers.IsAdmin(m.Author) {
+		_, err := s.ChannelMessageSend(m.ChannelID, "You do not have permission to use this command.")
+		helpers.LogError(err)
+		return
+	}
+
 	cmd, content := GetCommandAndContent(m.Content)
 	if cmd == "pester" {
 		mentions := m.Mentions
 		if len(mentions) < 1 {
-			_, _ = s.ChannelMessageSend(m.ChannelID, "You must mention somebody to pester...")
+			_, err := s.ChannelMessageSend(m.ChannelID, "You must mention somebody to pester...")
+			helpers.LogError(err)
 		}
 		_, err := pester.Create(m.Author.ID, mentions[0].ID, content)
 		if err != nil {
-			_, _ = s.ChannelMessageSend(m.ChannelID, "They are already being pestered.")
+			_, err = s.ChannelMessageSend(m.ChannelID, "They are already being pestered.")
+			helpers.LogError(err)
 			return
 		}
-		_, _ = s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("Now pestering %s", mentions[0].Username))
+		_, err = s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("Now pestering %s", mentions[0].Username))
+		helpers.LogError(err)
 	}
 
 	if cmd == "unpester" {
 		mentions := m.Mentions
 		if len(mentions) < 1 {
-			_, _ = s.ChannelMessageSend(m.ChannelID, "You need to mention somebody to stop pestering.")
+			_, err := s.ChannelMessageSend(m.ChannelID, "You need to mention somebody to stop pestering.")
+			helpers.LogError(err)
 		}
 		err := pester.DeleteByUidTo(mentions[0].ID)
 		helpers.LogError(err)
---
+++ helpers/config_helper.go
@@ -0,0 +1,41 @@
+package helpers
+
+import (
+	"encoding/json"
+	"github.com/bwmarrin/discordgo"
+	"io/ioutil"
+	"os"
+)
+
+type Admin struct {
+	ID string `json:"id"`
+}
+
+type Admins struct {
+	Admins []Admin `json:"admins"`
+}
+
+func GetAdmins() (out Admins) {
+	f, err := os.Open("config/admins.json")
+	LogError(err)
+
+	bVal, err := ioutil.ReadAll(f)
+	LogError(err)
+
+	err = json.Unmarshal(bVal, &out)
+	LogError(err)
+
+	return
+}
+
+func IsAdmin(user *discordgo.User) bool {
+	admins := GetAdmins().Admins
+
+	for _, v := range admins {
+		if v.ID == user.ID {
+			return true
+		}
+	}
+
+	return false
+}
